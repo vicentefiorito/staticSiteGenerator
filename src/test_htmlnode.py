99708a7969413a98248a867686fba77cb1162f2a
vicentefiorito <vicentefiorito@gmail.com>
2024-03-08 19:58:10 -0500

    implemented unit test for the ParentNode class functionality

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 class TestHTMLNode(unittest.TestCase):
     # HTML node simple tests
@@ -34,6 +34,53 @@ class TestHTMLNode(unittest.TestCase):
         nodeProps = node.to_html()
         expectedProps = '<h1 class="container">This is an H1 div</h1>'
         self.assertEqual(nodeProps,expectedProps)
+    
+    # Parent Node tests
+    def test_parent_node(self):
+        node = ParentNode(
+            "p",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ],
+        )
+
+        nodeProps = node.to_html()
+        expectedProps = '<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>'
+        self.assertEqual(nodeProps,expectedProps)
+    
+    def test_parent_node_no_tag(self):
+        node = ParentNode(
+            None,
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ],
+        )
+        self.assertRaises(ValueError,node.to_html)
+    
+    def test_parent_no_children(self):
+        node = ParentNode('b',None)
+        self.assertRaises(ValueError,node.to_html)
+    
+    def test_parent_double_tag(self):
+        child_node = LeafNode('a','Link Here!')
+        parent_node = ParentNode('h1',[child_node])
+        parent_HTML = parent_node.to_html()
+        expected_HTML = '<h1><a>Link Here!</a></h1>'
+        self.assertEqual(parent_HTML,expected_HTML)
+    
+    def test_parent_triple_tag(self):
+        grandchild_node = LeafNode('b','grandchild')
+        child_node = ParentNode('span',[grandchild_node])
+        parent_node = ParentNode('h1',[child_node])
+        parent_HTML = parent_node.to_html()
+        exceptedHTML = '<h1><span><b>grandchild</b></span></h1>'
+        self.assertEqual(parent_HTML,exceptedHTML)
 
 if __name__ == "__main__":
     unittest.main()
