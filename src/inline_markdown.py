ed39f2bcd3f524b54d97eda5cc1dbe9c7cfa39d1
vicentefiorito <vicentefiorito@gmail.com>
2024-03-12 10:41:46 -0400

    made implementation of a function that converts a raw string of markdown into a list of TextNode objects:

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -10,6 +10,16 @@ from textnode import (
     TextNode
 )
 
+# function that converts a raw string of Markdown into a list of TextNode objects
+def text_to_textnodes(text):
+    nodes = [TextNode(text,text_type_text)]
+    nodes = split_nodes_delimiter(nodes,"**",text_type_bold) #splits the bold nodes
+    nodes = split_nodes_delimiter(nodes,"*",text_type_italic) #splits the italic nodes
+    nodes = split_nodes_delimiter(nodes,"`",text_type_code) #splits the code nodes
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
+
 def split_nodes_delimiter(old_nodes,delimeter,text_type):
     # this is the list that is going to return after the delimeter
     new_nodes = []
