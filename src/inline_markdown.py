a1b4768c7d682e8385f7dfbb9fad773d751e56ff
vicentefiorito <vicentefiorito@gmail.com>
2024-03-12 10:08:29 -0400

    added functionality for splitting the remaining markdown elements, links and images

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -33,6 +33,53 @@ def split_nodes_delimiter(old_nodes,delimeter,text_type):
         new_nodes.extend(split_node)
     return new_nodes
 
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != text_type_text:
+            new_nodes.append(old_node)
+            continue
+        original_text = old_node.text
+        images = extract_markdown_images(original_text)
+        if len(images) == 0:
+            new_nodes.append(old_node)
+            continue
+        for image in images:
+            sections = original_text.split(f"![{image[0]}]({image[1]})", 1)
+            if len(sections) != 2:
+                raise ValueError('Invalid Markdown, image tag was not closed!')
+            if sections[0] != "":
+                 new_nodes.append(TextNode(sections[0],text_type_text))
+            new_nodes.append(TextNode(image[0],text_type_image,image[1]))
+            original_text = sections[1]
+        if original_text == "":
+            new_nodes.append(TextNode(original_text,text_type_text))
+    return new_nodes
+
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != text_type_text:
+            new_nodes.append(old_node)
+            continue
+        original_text = old_node.text
+        links = extract_markdown_links(original_text)
+        if len(links) == 0:
+            new_nodes.append(old_node)
+            continue
+        for link in links:
+            sections = original_text.split(f"![{link[0]}]({link[1]})", 1)
+            if len(sections) != 2:
+                raise ValueError('Invalid Markdown, link tag was not closed!')
+            if sections[0] != "":
+                 new_nodes.append(TextNode(sections[0],text_type_text))
+            new_nodes.append(TextNode(link[0],text_type_link,link[1]))
+            original_text = sections[1]
+        if original_text == "":
+            new_nodes.append(TextNode(original_text,text_type_text))
+    return new_nodes
+
 # takes raw text and return a list of tuples,
 # each tuple has the alt text and the URL of any markdown image
 def extract_markdown_images(text):
