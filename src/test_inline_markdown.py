a8882483dd2ccd27a4c537fdbd59ec934ac4b56b
vicentefiorito <vicentefiorito@gmail.com>
2024-03-12 10:44:50 -0400

    did test for the implementation of text_to_textnodes

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -4,7 +4,8 @@ from inline_markdown import (
     extract_markdown_images,
     extract_markdown_links,
     split_nodes_image,
-    split_nodes_link
+    split_nodes_link,
+    text_to_textnodes
 )
 
 from textnode import (
@@ -157,6 +158,26 @@ class TestInlineMarkdown(unittest.TestCase):
                 TextNode(" with text that follows", text_type_text),
             ],
             new_nodes,
+        )
+    
+    def test_text_to_textnodes(self):
+        nodes = text_to_textnodes(
+            "This is **text** with an *italic* word and a `code block` and an ![image](https://i.imgur.com/zjjcJKZ.png) and a [link](https://boot.dev)"
+        )
+        self.assertListEqual(
+            [
+                TextNode("This is ", text_type_text),
+                TextNode("text", text_type_bold),
+                TextNode(" with an ", text_type_text),
+                TextNode("italic", text_type_italic),
+                TextNode(" word and a ", text_type_text),
+                TextNode("code block", text_type_code),
+                TextNode(" and an ", text_type_text),
+                TextNode("image", text_type_image, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and a ", text_type_text),
+                TextNode("link", text_type_link, "https://boot.dev"),
+            ],
+            nodes,
         )
 
 
