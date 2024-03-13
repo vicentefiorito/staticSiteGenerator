d65bca23b4d6284dc835fbe4f93db02158446eb4
vicentefiorito <vicentefiorito@gmail.com>
2024-03-13 12:32:45 -0400

    final commit for now, might come to add some stuff later

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 from markdown_blocks import markdown_to_html_node
 
 def extract_title(markdown):
@@ -33,3 +34,13 @@ def generate_page(from_path, template_path,dest_path):
         os.makedirs(dest_dir_path,exist_ok=True)
     to_file = open(dest_path,'w')
     to_file.write(template)
+
+def generate_pages_recursive(dir_path_content,template_path,dest_dir_path):
+    for file in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content,file)
+        dest_path = os.path.join(dest_dir_path,file)
+        if os.path.isfile(from_path):
+            dest_path = Path(dest_path).with_suffix(".html")
+            generate_page(from_path,template_path,dest_path)
+        else:
+            generate_pages_recursive(from_path,template_path,dest_path)
