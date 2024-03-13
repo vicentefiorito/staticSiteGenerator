eb9eaecbf80512c915315e765c8563fc8c2a3a9b
vicentefiorito <vicentefiorito@gmail.com>
2024-03-13 10:56:55 -0400

    did a long implementation of converting any single type of block to a html_node based on their markdown rules

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,7 @@
+from htmlnode import ParentNode
+from inline_markdown import text_to_textnodes
+from textnode import text_node_to_html_node
+
 # types of markdown blocks
 block_type_paragraph = 'paragraph'
 block_type_heading = 'heading'
@@ -15,6 +19,32 @@ def markdown_to_blocks(markdown):
         string = string.strip()
         block_strings.append(string)
     return block_strings
+
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+    children = [] 
+    for block in blocks:
+        html_node = block_to_html(block)
+        children.append(html_node)
+    return ParentNode('div',children,None)
+
+# this function takes an specific type of blocks
+# and calls the specific function for the type of block
+def block_to_html(block):
+    block_type = block_to_block_type(block)
+    if block_type == block_type_paragraph:
+        return paragraph_to_html_node(block)
+    if block_type == block_type_code:
+        return code_to_html_node(block)
+    if block_type == block_type_heading:
+        return heading_to_html_node(block)
+    if block_type == block_type_quote:
+        return quote_to_html_node(block)
+    if block_type == block_type_ordered_list:
+        return ordered_list_to_html_node(block)
+    if block_type == block_type_unordered_list:
+        return unordered_list_to_html_node(block)
+    raise ValueError('Invalid block type!')
 
 # function that takes a single block of markdwon and return the type of block it is
 def block_to_block_type(block):
@@ -60,4 +90,67 @@ def block_to_block_type(block):
         return block_type_ordered_list
     
     return block_type_paragraph
-    
+
+# helper function that converts inline elements 
+# into html nodes to be processed by other functions
+def text_to_children(text):
+    text_nodes = text_to_textnodes(text)
+    children = []
+    for text_node in text_nodes:
+        html_node = text_node_to_html_node(text_node)
+        children.append(html_node)
+    return children
+
+def paragraph_to_html_node(block):
+    lines = block.split('\n')
+    paragraph = "".join(lines)
+    children = text_to_children(paragraph)
+    return ParentNode('p',children)
+
+def heading_to_html_node(block):
+    level = 0
+    for char in block:
+        if char == '#':
+            level += 1
+        else:
+            break
+    if level + 1 > len(block):
+        raise ValueError('Invalid header level: ${level}')
+    text = block[level + 1:]
+    children = text_to_children(text)
+    return ParentNode(f'h{level}',children)
+
+def code_to_html_node(block):
+    if not block.startswith('```') or not block.endswith('```'):
+        raise ValueError('Invalid code block')
+    text = block[4:-3]
+    children = text_to_children(text)
+    return ParentNode('pre',ParentNode('code',children))
+
+def unordered_list_to_html_node(block):
+    items = block.split('\n')
+    children_nodes = []
+    for item in items:
+        text = item[2:]
+        children = text_to_textnodes(text)
+        children_nodes.append(ParentNode('li',children))
+    return ParentNode('ul',children_nodes)
+
+def ordered_list_to_html_node(block):
+    items = block.split('\n')
+    children_nodes = []
+    for item in items:
+        text = item[3:]
+        children = text_to_textnodes(text)
+        children_nodes.append(ParentNode('li',children))
+    return ParentNode('ol',children_nodes)
+
+def quote_to_html_node(block):
+    lines = block.split('\n')
+    children = []
+    for line in lines:
+        if not line.startswith('>'):
+            raise ValueError('invalid quote block')
+        line_text = line.lstrip('>').strip
+        children.extend(text_to_children(line_text))
+    return ParentNode('blockquote',children)
--- src/textnode.py
+++ src/textnode.py
@@ -20,17 +20,17 @@ class TextNode:
     def __repr__(self):
         return f'TextNode({self.text}, {self.text_type}, {self.url})'
     
-    def text_node_to_html_node(text_node):
-        if text_node.text_type == text_type_text:
-            return LeafNode(None,text_node.text)
-        if text_node.text_type == text_type_bold:
-            return LeafNode('b',text_node.text)
-        if text_node.text_type == text_type_italic:
-            return LeafNode('i',text_node.text)
-        if text_node.text_type == text_type_code:
-            return LeafNode('code',text_node.text)
-        if text_node.text_type == text_type_link:
-            return LeafNode('a',text_node.text,{'href':text_node.url})
-        if text_node.text_type == text_type_image:
-            return LeafNode('img','',{'src':text_node.url,'alt':text_node.text})
-        raise Exception(f'Invalid TextNode option:{text_node.text_type}')
+def text_node_to_html_node(text_node):
+    if text_node.text_type == text_type_text:
+        return LeafNode(None,text_node.text)
+    if text_node.text_type == text_type_bold:
+        return LeafNode('b',text_node.text)
+    if text_node.text_type == text_type_italic:
+        return LeafNode('i',text_node.text)
+    if text_node.text_type == text_type_code:
+        return LeafNode('code',text_node.text)
+    if text_node.text_type == text_type_link:
+        return LeafNode('a',text_node.text,{'href':text_node.url})
+    if text_node.text_type == text_type_image:
+        return LeafNode('img','',{'src':text_node.url,'alt':text_node.text})
+    raise Exception(f'Invalid TextNode option:{text_node.text_type}')
