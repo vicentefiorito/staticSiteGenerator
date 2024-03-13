fcfb300dca5455bd543fca35645c6233b0f28955
vicentefiorito <vicentefiorito@gmail.com>
2024-03-13 11:42:40 -0400

    finalizing static page creation

--- src/gencontent.py
+++ src/gencontent.py
@@ -7,3 +7,29 @@ def extract_title(markdown):
         if line.startswith('# '):
             return line[2:]
     raise ValueError('No header provided')
+
+def generate_page(from_path, template_path,dest_path):
+    print(f'Generating page from {from_path} to {dest_path} using {template_path}')
+    from_file = open(from_path,'r')
+    markdown_content = from_file.read()
+    from_file.close()
+
+    template_file = open(template_path,'r')
+    template = template_file.read()
+    template_file.close()
+    
+    # converts the markdown of the from_file to html
+    nodes = markdown_to_html_node(markdown_content)
+    html = nodes.to_html()
+
+    # extracts the title of the markdown
+    title = extract_title(markdown_content)
+    template = template.replace('{{Title}}',title)
+    template = template.replace('{{Content}}',html)
+
+    # writes the new HTML to a file at dest_path
+    dest_dir_path = os.path.dirname(dest_path)
+    if dest_dir_path != "":
+        os.makedirs(dest_dir_path,exist_ok=True)
+    to_file = open(dest_path,'w')
+    to_file.write(template)
